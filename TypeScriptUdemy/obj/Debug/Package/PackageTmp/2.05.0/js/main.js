e2b335ca765681b7613a8ad306019d41c9cd42dc
Dániel Némethy <nemethy.daniel@informa.hu>
2019-12-22 22:30:42 +0100

    commit project 2

--- TypeScriptUdemy/obj/Debug/Package/PackageTmp/2.05.0/js/main.js
+++
@@ -1,9 +0,0 @@
-/*
-*    main.js
-*    Mastering Data Visualization with D3.js
-*    2.5 - Activity: Adding SVGs to the screen
-*/
-var cha = d3.select("#chart-area");
-var cvs = chs.append("svg").attr("width", 500).attr("height", 400);
-var lin = cvs.append("line").attr("x1", 10).attr("x2", 490).attr("y1", 400).attry("y2", 390);
-
